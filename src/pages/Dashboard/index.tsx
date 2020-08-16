48f7bba1b56cabb1540e987cbc66e5ee96f9fa1c
Fabio Picoli Jr <fabiopicolijr@gmail.com>
2020-08-16 18:56:50 -0300

    challenge

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -54,12 +54,27 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    navigation.navigate(`FoodDetails`, { id });
   }
 
   useEffect(() => {
+    // @study: filter automatico "like" no server-json
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
+      const response = await api.get<Food[]>('/foods', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        },
+      });
+
+      const foodsList: Food[] = response.data;
+
+      const foodsListFormmatedPrice = foodsList.map(food => ({
+        ...food,
+        formattedPrice: formatValue(food.price),
+      }));
+
+      setFoods(foodsListFormmatedPrice);
     }
 
     loadFoods();
@@ -67,14 +82,20 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
+      const response = await api.get<Category[]>('categories');
+      setCategories(response.data);
     }
 
     loadCategories();
   }, []);
 
   function handleSelectCategory(id: number): void {
-    // Select / deselect category
+    setSelectedCategory(state => {
+      if (state === id) {
+        return undefined;
+      }
+      return id;
+    });
   }
 
   return (
--- src/pages/FoodDetails/index.tsx
+++ src/pages/FoodDetails/index.tsx
@@ -74,6 +74,17 @@ const FoodDetails: React.FC = () => {
   useEffect(() => {
     async function loadFood(): Promise<void> {
       // Load a specific food with extras based on routeParams id
+      const response = await api.get<Food>(`foods/${routeParams.id}`);
+      const foodResponse = response.data;
+
+      setExtras(foodResponse.extras.map(extra => ({ ...extra, quantity: 0 })));
+
+      const foodWithFormmatedPrice = {
+        ...foodResponse,
+        formattedPrice: formatValue(foodResponse.price),
+      };
+
+      setFood(foodWithFormmatedPrice);
     }
 
     loadFood();
@@ -81,30 +92,59 @@ const FoodDetails: React.FC = () => {
 
   function handleIncrementExtra(id: number): void {
     // Increment extra quantity
+    setExtras(state =>
+      state.map(extra => {
+        if (extra.id === id) {
+          return { ...extra, quantity: extra.quantity + 1 };
+        }
+        return extra;
+      }),
+    );
   }
 
   function handleDecrementExtra(id: number): void {
     // Decrement extra quantity
+    setExtras(state =>
+      state.map(extra => {
+        if (extra.id === id) {
+          if (extra.quantity === 0) {
+            return extra;
+          }
+          return { ...extra, quantity: extra.quantity - 1 };
+        }
+        return extra;
+      }),
+    );
   }
 
   function handleIncrementFood(): void {
     // Increment food quantity
+    setFoodQuantity(state => state + 1);
   }
 
   function handleDecrementFood(): void {
     // Decrement food quantity
+    setFoodQuantity(state => (state === 1 ? 1 : state - 1));
   }
 
   const toggleFavorite = useCallback(() => {
     // Toggle if food is favorite or not
+    setIsFavorite(state => !state);
   }, [isFavorite, food]);
 
   const cartTotal = useMemo(() => {
     // Calculate cartTotal
+    const extraTotal = extras.reduce((x, y) => x + y.quantity * y.value, 0);
+
+    return formatValue((extraTotal + food.price) * foodQuantity);
   }, [extras, food, foodQuantity]);
 
   async function handleFinishOrder(): Promise<void> {
     // Finish the order and save on the API
+    await api.post('orders', {
+      ...food,
+      extras,
+    });
   }
 
   // Calculate the correct icon name
@@ -112,6 +152,20 @@ const FoodDetails: React.FC = () => {
     () => (isFavorite ? 'favorite' : 'favorite-border'),
     [isFavorite],
   );
+
+  useLayoutEffect(() => {
+    // Add the favorite icon on the right of the header bar
+    navigation.setOptions({
+      headerRight: () => (
+        <MaterialIcon
+          name={favoriteIconName}
+          size={24}
+          color="#FFB84D"
+          onPress={() => toggleFavorite()}
+        />
+      ),
+    });
+  }, [navigation, favoriteIconName, toggleFavorite]);
 
   useLayoutEffect(() => {
     // Add the favorite icon on the right of the header bar
